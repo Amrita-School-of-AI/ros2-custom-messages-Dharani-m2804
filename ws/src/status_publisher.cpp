5807b2d1a4ad2f480ca50508cf49daa9bb85ee4f
Dharani <dharani_m2804@github.com>
2026-02-22 00:42:15 +0530

    Completed ROS2 Assignment 7 - Custom Messages

--- ws/src/status_publisher.cpp
+++
@@ -1,6 +0,0 @@
-RCLCPP_INFO(this->get_logger(),
-  "Publishing: robot=%s, battery=%.1f, active=%s, missions=%d",
-  message.robot_name.c_str(),
-  message.battery_level,
-  message.is_active ? "true" : "false",
-  message.mission_count);
